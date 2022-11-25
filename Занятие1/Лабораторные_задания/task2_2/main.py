b870d3c80130c86649ac69ad416f1795ca7fd9d8
tatlisitchuk <tankin-yashik@mail.ru>
2022-11-25 20:54:49 +0300

    2_2

--- Занятие1/Лабораторные_задания/task2_2/main.py
+++ Занятие1/Лабораторные_задания/task2_2/main.py
@@ -2,7 +2,8 @@ def task() -> int:
     list_numbers = [8, -8, 5, 5, -4, -7, -1, -3, 4, 8, -8, -3, -3, 4, 5, -5, 1, -7, 3, -2]
     return sum(filter(lambda x: x < 0, list_numbers))
     # return sum([num for num in list_numbers if num < 0])  # TODO заменить list comprehension на filter
-
+#bgtbgtbgtbgt
 
 if __name__ == "__main__":
     print(task())
+
